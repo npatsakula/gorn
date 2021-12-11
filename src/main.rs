8cb46d6bd37d034db4bfbddddb47f1c7792c841a
Patsakula Nikita <nikita.patsakula@gmail.com>
2021-12-11 23:00:59 +0300

    WIP: [*] Duplicates-finder implemented. Large file-tree bug fixed.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use blake2::{Blake2b, Digest};
+use blake2::{Blake2b512, Digest};
 use clap::Parser;
 use rayon::prelude::*;
-use std::{collections::BTreeMap, fs::File, io::Read, path::Path};
+use std::{
+    collections::{BTreeMap, BTreeSet, HashMap},
+    fs::{File, OpenOptions},
+    io::{self, Read},
+    path::{Path, PathBuf},
+};
 use walkdir::WalkDir;
 
 #[derive(Debug, Parser)]
@@ -21,54 +26,53 @@ struct Opt {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = Opt::parse();
+    let Opt {
+        path,
+        print_tree,
+        common,
+    } = Opt::parse();
 
-    let hashes = build_file_tree(&opts.path).map(calculate_hashes::<Blake2b>)?;
-    let merged = merge_hashes(hashes.clone());
-    let string_hashes = print_hashes(hashes);
+    let paths = extract_paths(&path)?;
+    let hashes = calculate_hashes::<Blake2b512>(paths)?;
 
-    if opts.print_tree {
+    let merged = merge_hashes(hashes.values().cloned());
+    let string_hashes = stringify_hashes(hashes);
+
+    if print_tree {
         println!("{:#?}", string_hashes);
     }
 
-    if opts.common {
-        println!("{:#?}", find_commons(string_hashes));
+    if common {
+        println!("{:#?}", find_duplicates(&string_hashes));
     }
 
     println!("{:02x}", &merged.finalize());
     Ok(())
 }
 
-fn find_commons(string_hashes: BTreeMap<String, String>) -> (i32, Vec<[String; 2]>) {
-    let mut commons = Vec::new();
-    let mut counter = 0;
+fn find_duplicates(hashes: &BTreeMap<PathBuf, String>) -> BTreeMap<&str, Vec<&Path>> {
+    let mut result: HashMap<&str, Vec<&Path>> = HashMap::with_capacity(hashes.len());
 
-    for (file_name1, hash1) in string_hashes.iter() {
-        for (file_name2, hash2) in string_hashes.iter() {
-            if &hash1 == &hash2 {
-                counter += 1;
-                commons.push([file_name1.to_owned(), file_name2.to_owned()])
-            }
-        }
+    for (path, hash) in hashes {
+        result.entry(hash).or_default().push(path);
     }
 
-    (counter, commons)
+    result
+        .into_iter()
+        .filter(|(_hash, paths)| paths.len() > 1)
+        .collect()
 }
 
-fn build_file_tree(path: &Path) -> std::io::Result<BTreeMap<String, File>> {
-    let metadata = path.metadata()?;
-    if metadata.is_file() {
-        let file = File::open(path)?;
-        return Ok(BTreeMap::from([(path.to_str().unwrap().to_owned(), file)]));
+fn extract_paths(path: &Path) -> io::Result<BTreeSet<PathBuf>> {
+    let mut result = BTreeSet::new();
+    if path.metadata()?.is_file() {
+        result.insert(path.to_owned());
+        return Ok(result);
     }
 
-    let mut result = BTreeMap::new();
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        let metadata = entry.metadata()?;
-
-        if metadata.is_file() {
-            let file = File::open(entry.path())?;
-            result.insert(entry.path().to_str().unwrap().to_owned(), file);
+        if entry.metadata()?.is_file() {
+            result.insert(entry.path().to_owned());
         }
     }
 
@@ -76,35 +80,60 @@ fn build_file_tree(path: &Path) -> std::io::Result<BTreeMap<String, File>> {
 }
 
 fn calculate_hashes<H: Digest + Default + Send>(
-    source: BTreeMap<String, File>,
-) -> BTreeMap<String, H> {
+    source: BTreeSet<PathBuf>,
+) -> io::Result<BTreeMap<PathBuf, H>> {
     source
         .into_par_iter()
-        .map(|(path, file)| (path, hash::<H>(file)))
-        .collect()
+        .map(|path| {
+            let file = OpenOptions::new()
+                .read(true)
+                .write(false)
+                .create(false)
+                .open(&path)?;
+
+            Ok((path, hash::<H>(file)))
+        })
+        .try_fold(
+            || BTreeMap::new(),
+            |mut acc: BTreeMap<PathBuf, H>, val: io::Result<(PathBuf, H)>| {
+                let (path, hash) = val?;
+                acc.insert(path, hash);
+                Ok(acc)
+            },
+        )
+        .try_reduce(
+            || BTreeMap::new(),
+            |mut acc, mut val| {
+                acc.append(&mut val);
+                Ok(acc)
+            },
+        )
 }
 
-fn print_hashes<H: Digest>(source: BTreeMap<String, H>) -> BTreeMap<String, String> {
+fn stringify_hashes<T: Ord + Send, H: Digest + Send>(
+    source: BTreeMap<T, H>,
+) -> BTreeMap<T, String> {
     source
-        .into_iter()
-        .map(|(path, hasher)| (path, hex::encode(hasher.finalize())))
+        .into_par_iter()
+        .map(|(p, hasher)| (p, hex::encode(hasher.finalize())))
         .collect()
 }
 
-fn merge_hashes<H: Digest + Default>(source: BTreeMap<String, H>) -> H {
-    source.into_iter().fold(H::default(), |mut acc, (_, hash)| {
+fn merge_hashes<I: IntoIterator<Item = H>, H: Digest + Default>(source: I) -> H {
+    source.into_iter().fold(H::default(), |mut acc, hash| {
         acc.update(hash.finalize());
         acc
     })
 }
 
 fn hash<H: Digest + Default>(mut file: File) -> H {
-    let mut buffer = [0u8; 2048];
+    const BUFFER_SIZE: usize = 4086;
+    let mut buffer = [0u8; BUFFER_SIZE];
     let mut hasher = H::default();
 
     while let Ok(n) = file.read(&mut buffer) {
         hasher.update(&buffer[..n]);
-        if n == 0 || n < 1024 {
+        if n < BUFFER_SIZE {
             break;
         }
     }
